2956ddbc0838906b6eecf1d1f95eb8602d28f8ec
xiachao <776735626@qq.com>
2020-07-30 17:21:33 +0800

    init

--- src/views/phone.vue
+++ src/views/phone.vue
@@ -1,29 +1,22 @@
 <template>
     <div class="main">
-        <div class="center" style="line-height: 50px;height: 50px;background: #fff;">懒人打卡</div>
-        <div v-if="isLogin">
-                <van-form label-width="80px" style="margin-top: 20vh" @submit="login">
-                    <van-field
-                        v-model="loginId"
-                        name="loginId"
-                        label="OA用户名"
-                        placeholder="OA用户名"
-                        :rules="[{ required: true, message: '请填写OA用户名' }]"
-                    />
-                    <div style="margin: 16px;">
-                        <van-button round block type="info" native-type="submit">
-                            登录
-                        </van-button>
-                    </div>
-                </van-form>
-        </div>
-        <div class="center" style="margin-top: 20vh" v-else>
+        <el-card class="login-box" v-if="isLogin">
+            <el-form label-width="88px">
+                <el-form-item :rules="[{ required: true, message: '请填写OA用户名' }]" label="OA用户名">
+                    <input class="input" type="text" v-model="loginId" placeholder="请填写OA用户名">
+                </el-form-item>
+            </el-form>
+            <div class="login-btn" @click="login">
+                登 录
+            </div>
+        </el-card>
+        <el-card class="login-box center" v-else>
             <div v-show="0">{{sign}}</div>
             <p>OA账号：{{this.loginId}}</p><br>
             <p>打卡状态：{{user.enable|getName}}</p>
             <div class="btn" @click="changeState(1)" v-show="!user.enable">启用</div>
             <div class="btn" @click="changeState(0)" v-show="user.enable">停用</div>
-        </div>
+        </el-card>
     </div>
 </template>
 
@@ -51,9 +44,7 @@ export default {
 
     },
     methods: {
-        async login(values){
-            console.log('submit', values);
-            this.flag = true;
+        async login(){
             let res = await this.$http.post('/sys/userLogin',{loginId:this.loginId});
             if(res.state){
                 this.$message.success('登陆成功');
@@ -99,8 +90,30 @@ export default {
         margin: 0 auto;
         line-height: 80px;
         margin-top: 5vh;
-        background: #fff;
-        border: 1px solid #dfdfdf;
+        background: #409eff;
+        color: #fff;
         border-radius: 50%;
+    }
+    .login-box{
+        border-radius: 20px;
+        padding: 8vh 0;
+        width: 90%;
+        margin: 25vh auto;
+    }
+    .login-btn{
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        border-radius: 20px;
+        background: #409eff;
+        color: #fff;
+        border: 0;
+        text-align: center;
+        margin-top: 10vh;
+    }
+    .input{
+        border: 0;
+        border-bottom: 1px solid #dfdfdf;
+        width: 100%;
     }
 </style>
